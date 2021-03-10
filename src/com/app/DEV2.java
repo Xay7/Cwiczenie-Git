729123cc89d5617ccb4b25326f181c13676366c2
goldentraingt <goldentraingt@gmail.com>
2021-03-10 14:50:38 +0100

    Branch DEV2 added.

--- src/com/app/DEV2.java
+++ src/com/app/DEV2.java
@@ -1,7 +1,5 @@
 package com.app;
 
 public class DEV2 {
-	public static void main(String []args){
-		  System.out.println("DEV2 created by Rafał Bujewski");
-		}
+	public String author = "Rafał Bujewski";
 }
