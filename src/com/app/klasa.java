c3948fbc98496e8b1400c29f5028796f00e7e89b
goldentraingt <goldentraingt@gmail.com>
2021-03-10 10:32:50 +0100

    Pierwszy commit

---
+++ src/com/app/klasa.java
@@ -0,0 +1,12 @@
+package com.app;
+
+public class klasa {
+
+	public static void main(String []args){
+	  System.out.println(System.getProperty("java.vendor"));
+	  System.out.println(System.getProperty("java.vendor.url"));
+	  System.out.println(System.getProperty("java.version"));
+	  System.out.println("Hello World");
+	}
+
+}
